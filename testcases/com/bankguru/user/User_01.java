0deee2973da516f4a217d8aa981d142f25b9dacf
KhangD <Khang.DaoD@homecredit.vn>
2022-02-23 22:09:49 +0700

    first commit

---
+++ testcases/com/bankguru/user/User_01.java
@@ -0,0 +1,10 @@
+package com.bankguru.user;
+
+import org.testng.annotations.Test;
+
+public class User_01 {
+    @Test
+    public void User_01() {
+        System.out.println("Hello World!");
+    }
+}
